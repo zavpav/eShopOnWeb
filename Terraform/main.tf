84d30e443cbe8428cb10128c370a7fd0a036feff
Zavjalov <zavjalov@gmail.com>
2023-03-27 01:42:56 +0400

    Add Key vault

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -13,7 +13,7 @@ provider azurerm {
 }
 
 resource azurerm_resource_group res_group {
-  name     = "pzaz_esw1"
+  name     = "pzaz_esw"
   location = "West Europe"
 }
 
@@ -28,13 +28,90 @@ resource random_string db_postfix {
   upper = false
 }
 
+
+###################################################################################################
+
+data azurerm_client_config current {}
+
+resource random_string connect_pass {
+  length = 8
+  special = true
+  upper = true
+}
+
+resource azurerm_key_vault kv_webapp {
+  name = "key-vault-pzaz-${random_string.db_postfix.result}"
+  resource_group_name = local.res_grp
+  location = local.location
+  tenant_id = data.azurerm_client_config.current.tenant_id
+  sku_name = "standard"
+  
+  access_policy {
+    tenant_id = data.azurerm_client_config.current.tenant_id
+    object_id = data.azurerm_client_config.current.object_id
+    secret_permissions = [
+      "Set",
+      "Get",
+      "Delete",
+      "Purge",
+      "Recover",
+      "List"
+    ]
+  }
+}
+
+/* resource azurerm_key_vault_access_policy kv {
+  key_vault_id = azurerm_key_vault.kv.id
+  tenant_id    = data.azurerm_client_config.current.tenant_id
+  object_id    = data.azurerm_client_config.current.object_id
+
+  key_permissions = [
+    "Get",
+  ] 
+
+  secret_permissions = [
+    "Get",
+  ]
+} */
+/* 
+resource azurerm_key_vault_access_policy kv_web_app {
+  key_vault_id = azurerm_key_vault.kv_webapp.id
+  tenant_id    = data.azurerm_client_config.current.tenant_id
+  object_id    = azurerm_windows_web_app.web_app.identity[0].principal_id
+
+  secret_permissions = [
+    "Get",
+  ]
+} */
+
+
+
+resource azurerm_key_vault_secret kv_pass_catalog {
+  key_vault_id = azurerm_key_vault.kv_webapp.id
+  name = "SQLPasswordCatalog"
+  value = random_string.connect_pass.result
+}
+
+
+resource azurerm_key_vault_secret kv_catalog_connection_string {
+  key_vault_id = azurerm_key_vault.kv_webapp.id
+  name = "SQLCatalogConnectionString"
+  value = "Data Source=tcp:${azurerm_mssql_server.db_srv.fully_qualified_domain_name},1433;Initial Catalog=${azurerm_mssql_database.db_db.name};User Id=${azurerm_mssql_server.db_srv.administrator_login};Password='${random_string.connect_pass.result}';"
+}
+resource azurerm_key_vault_secret kv_identity_connection_string {
+  key_vault_id = azurerm_key_vault.kv_webapp.id
+  name = "SQLIdentityConnectionString"
+  value = "Data Source=tcp:${azurerm_mssql_server.db_srv.fully_qualified_domain_name},1433;Initial Catalog=${azurerm_mssql_database.db_idnt.name};User Id=${azurerm_mssql_server.db_srv.administrator_login};Password='${random_string.connect_pass.result}';"
+}
+
+###################################################################################################
 resource azurerm_mssql_server db_srv {
   name = "pzaz-sql-${random_string.db_postfix.result}" 
   resource_group_name = local.res_grp
   location = local.location
   version = "12.0"
   administrator_login = "adminlogin"
-  administrator_login_password = "paad123!!!"
+  administrator_login_password = random_string.connect_pass.result
 }
 
 resource azurerm_mssql_firewall_rule db_srv_firewall {
@@ -69,16 +146,22 @@ resource azurerm_windows_web_app web_app {
     resource_group_name = local.res_grp
     location = local.location
     service_plan_id = azurerm_service_plan.web_service_plan.id
+  #  key_vault_reference_identity_id = azurerm_key_vault.kv_webapp.id
 
+
+    identity {
+      type = "SystemAssigned"
+    }
+    #https://learn.microsoft.com/en-us/azure/app-service/app-service-key-vault-references?tabs=azure-cli
     connection_string {
         name="CatalogConnection"
         type="SQLAzure"
-        value="Data Source=tcp:${azurerm_mssql_server.db_srv.fully_qualified_domain_name},1433;Initial Catalog=${azurerm_mssql_database.db_db.name};User Id=${azurerm_mssql_server.db_srv.administrator_login};Password='${azurerm_mssql_server.db_srv.administrator_login_password}';"
+        value="@Microsoft.KeyVault(VaultName=${azurerm_key_vault.kv_webapp.name};SecretName=${azurerm_key_vault_secret.kv_catalog_connection_string.name})"
     }
     connection_string {
         name="IdentityConnection"
         type="SQLAzure"
-        value="Data Source=tcp:${azurerm_mssql_server.db_srv.fully_qualified_domain_name},1433;Initial Catalog=${azurerm_mssql_database.db_idnt.name};User Id=${azurerm_mssql_server.db_srv.administrator_login};Password='${azurerm_mssql_server.db_srv.administrator_login_password}';"
+        value="@Microsoft.KeyVault(VaultName=${azurerm_key_vault.kv_webapp.name};SecretName=${azurerm_key_vault_secret.kv_identity_connection_string.name})"
     }
        
     site_config {
@@ -89,15 +172,35 @@ resource azurerm_windows_web_app web_app {
         } 
 
     }
+
     app_settings = {
-        "ASPNETCORE_ENVIRONMENT" = "Development" 
+        "ASPNETCORE_ENVIRONMENT" = "Development"
+        "KEY_VAULT" =  azurerm_key_vault.kv_webapp.id
+        "TestKeyValut"="@Microsoft.KeyVault(VaultName=${azurerm_key_vault.kv_webapp.name};SecretName=${azurerm_key_vault_secret.kv_pass_catalog.name})"
     }
   
 }
 
+###################################################################################################
+
+resource azurerm_key_vault_access_policy kv_to_webapp {
+  key_vault_id = azurerm_key_vault.kv_webapp.id
+  tenant_id    = data.azurerm_client_config.current.tenant_id
+  object_id    = azurerm_windows_web_app.web_app.identity[0].principal_id
+
+  key_permissions = [
+    "Get",
+  ] 
+
+  secret_permissions = [
+    "Get",
+  ]
+} 
+
+
 resource null_resource deploy_web_zip {
     provisioner "local-exec" {
-        command = "az webapp deploy --resource-group ${local.res_grp} --name ${azurerm_windows_web_app.web_app.name} --src-path ../src/Web/obj/Release/net7.0/PubTmp/Web-20230324185459081.zip"
+        command = "az webapp deploy --resource-group ${local.res_grp} --name ${azurerm_windows_web_app.web_app.name} --src-path ../src/Web/obj/Release/net7.0/PubTmp/Web-20230325180426612.zip"
         interpreter = ["PowerShell", "-Command"]
     }
 
